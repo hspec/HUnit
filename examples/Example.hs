c5c99736cb50766d2b534edb736e2239c1b39981
panne <none>
2004-03-27 15:42:45 +0100

    [project @ 2004-03-27 14:42:45 by panne]
    Warning police: Added type signatures and changed the type of main to
    the more common "IO ()".
    
    darcs-hash:20040327144245-a106c-66d31dd8638145a5765b96ea9638e7cd12eaf3bd

--- examples/Example.hs
+++ examples/Example.hs
@@ -14,21 +14,25 @@ partA v = return (v+2, v+3)
 partB :: Int -> IO Bool
 partB v = return (v > 5)
 
-
+test1 :: Test
 test1 = TestCase (assertEqual "for (foo 3)," (1,2) (foo 3))
 
+test2 :: Test
 test2 = TestCase (do (x,y) <- partA 3
                      assertEqual "for the first result of partA," 5 x
                      b <- partB y
                      assertBool ("(partB " ++ show y ++ ") failed") b)
 
+tests :: Test
 tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
 
-
+tests' :: Test
 tests' = test [ "test1" ~: "(foo 3)" ~: (1,2) ~=? (foo 3),
                 "test2" ~: do (x, y) <- partA 3
                               assertEqual "for the first result of partA," 5 x
                               partB y @? "(partB " ++ show y ++ ") failed" ]
 
+main :: IO ()
 main = do runTestTT tests
           runTestTT tests'
+          return ()
